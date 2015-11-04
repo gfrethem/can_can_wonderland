5f54eb22f2c088ff4ea5037a1f76b80c9f61fc5f
David Beck <beckx400@d.umn.edu>
2015-11-04 10:24:24 -0600

    File Structure Created

---
+++ README.md
@@ -0,0 +1,1 @@
+# can_can_wonderland
---
+++ app.js
@@ -0,0 +1,19 @@
+//REQUIRED LIBRARIES
+var express = require('express');
+var path = require('path');
+
+//REQUIRED ROUTES
+var index = require('./routes/index');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+var server = app.listen(3000, function(){
+    var port = server.address().port;
+    console.log('Listening on port: ', port);
+});
+
+app.use('/', index);
+
+module.exports = app;
