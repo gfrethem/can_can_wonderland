1d0aaf91ed59073e400a69df3247d1ea840cbf0c
lizpiet <lizziepowers55@gmail.com>
2015-11-05 16:57:50 -0600

    added passport

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 var express = require('express');
 var path = require('path');
 
+//REQUIRE PASSPORT, etc --Liz
+var passport = require('passport');
+var session = require('express-session');
+var localStrategy =require ('localStrategy');
+//WE DON"T KNOW HOW TO HANDLE THIS YET // MONGOOSE STUFF
+//var User = require('../models/user');
+
+
 //REQUIRED ROUTES
 var index = require('./routes/index');
 
@@ -14,6 +22,61 @@ var server = app.listen(3000, function(){
     console.log('Listening on port: ', port);
 });
 
+
+//PASSPORT USE --Liz
+passport.use('local', new localStrategy({
+        passReqToCallback:true,
+        usernameField:'username',
+        passwordField:'password'},
+    function(req, username, password, done) {
+
+        User.findOne({ username: username}, function(err, user){
+            if (err) throw err;
+            if (!user)
+                return done (null, false, {message: 'This message is crazy.'});
+            //test matching passwords
+            user.comparePassword(password, function(err, isMatch){
+                if (err) throw err;
+                if(isMatch)
+                    return done(null, user);
+                else
+                    done(null, false, { message:'This message is cray cray.'
+                    });
+            });
+        });
+    }));
+
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, callback){
+    User.findById(id, function(err, user){
+        if(err) callback(err);
+        callback(null, user)
+    })
+});
+
+/////////////////
+
 app.use('/', index);
+
+//USE SESSION --Liz
+app.use(session({
+    secret:'secret',
+    key:'user',
+    resave:'true',
+    saveUninitialized: false,
+    cookie: {maxAge: null, secure:false}
+}));
+
+//USE PASSPORT --Liz
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('local', new localStrategy({ passReqToCallBack : true, usernameField: 'username'},
+        function(req, username, password, done){
+
+        }));
 
 module.exports = app;
---
+++ user.js
@@ -0,0 +1,85 @@
+/**
+ * Created by Liz on 11/5/15.
+ */
+
+UserSchema.pre('save', function(next){
+    var user = this;
+
+//only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+//generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTORY, function(err,salt) {
+        if (err) return next(err);
+//hash the password a long with our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+//override the clear text password with the hashed one
+            user.password = hash;
+            next();
+        })
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch)
+    {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+};
+
+
+
+
+//THIS ROUTE IS FOR THE FORM/////
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require ('path');
+
+/* GET page. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../public/views/index.html'));
+});
+
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/users/home',
+    failureRedirect: '/'
+}));
+
+module.exports = router;
+
+
+//THIS ROUTE IS FOR THE REGISTRATION FORM/////
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+var Users = require('../../models/user');
+
+router.get('/registerLanding', function(req, res, next){
+    res.sendFile(path.join(__dirname, '../public/views/registerLanding.html'));
+});
+
+router.post('/', function(req, res, next){
+    Users.create(req.body, function(err, post){
+        if (err)
+            next(err);
+        else
+            res.redirect('register/registerLanding');
+    });
+});
+
+module.exports = router;
+
+//DON'T FORGET TO ADD ROUTES AND CHANGE NAMES!!!!!!!
+
+var register = require('./routes/register');
+
+app.use('/register', register);
+
+//Goes in users.js
+router.get('/', function(req, res, next) {
+    res.json(req.isAuthenticated());
+});
+
