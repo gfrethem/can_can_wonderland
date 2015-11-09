e6edd62961c81de6c2f9c8fa094dd7cc6383b122
lizpiet <lizziepowers55@gmail.com>
2015-11-09 16:06:07 -0600

    added angular jazz to register calendar

--- public/javascripts/registerView.js
+++ public/javascripts/registerView.js
@@ -1,6 +1,33 @@
-var app = angular.module('frontDeskApp', ['xeditable']);
+var app = angular.module('frontDeskApp', []);
 
 app.controller('FrontDeskController', ["$scope", function($scope){
     var vm = this;
     vm.message = 'Front Desk View';
+    vm.quarterHour = true;
+    vm.showReservationSlot = true;
+    vm.totalAvailableSlots = 20;
+    vm.partySize = 0;
+    vm.fullname = 'open';
+    vm.fullhour = vm.hours + vm.quarters;
+    vm.fullhourSlotNumber = - vm.totalAvailableSlots - vm.peopleSignedUp;
+    vm.walkup = false;
+    vm.checkin = false;
+    vm.reservations= {
+        fullname: "Just Stop!!",
+        fullhourSlotNumber: "5",
+        partySize: 6,
+        message: "what!!!",
+        quarterHour: 10
+    };
+
+    //database call;
+
+    vm.showQuarterHour = function(){
+        vm.quarterHour = !vm.quarterHour;
+    };
+
+    vm.showReservation = function(){
+        vm.showReservationSlot = !vm.showReservationSlot;
+    };
+
 }]);
