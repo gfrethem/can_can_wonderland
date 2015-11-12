d02cccbe8f5a151db571cf773da0c0f39cbaeed5
lizpiet <lizziepowers55@gmail.com>
2015-11-12 17:13:33 -0600

    css mega changes

--- public/javascripts/customerView.js
+++ public/javascripts/customerView.js
@@ -157,6 +157,39 @@ app.controller("CustomerCalendarController", ["$scope", "captureRes", "numSlots"
     }
 }]);
 
+//////
+
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+
+//////
+
+
+
 app.controller("ConfirmController", ["$scope", "captureRes", "numSlots", function($scope, captureRes, numSlots){
     var vm = this;
     var selectedDate = captureRes.newReservation.datetime;
