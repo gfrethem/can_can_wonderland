85878316793f746243e8a4ab8007ebb1444642ae
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-11-17 13:37:18 -0600

    Typo in routes/login.js

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ var path = require('path');
 var passport = require('passport');
 
 router.get('/', function(req, res, next){
-    res.sendFile(path.join(__dirname, '../publiac/views/index.html'));
+    res.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
 router.post('/', passport.authenticate('local',
