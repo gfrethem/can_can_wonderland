c87eb60242037aeb378cf7b66904d17bd87f6d9f
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-11-18 15:54:18 -0600

    SO CLOSE on Facebook Auth

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var User = models.User;
 var passport = require('passport');
 var session = require('express-session');
 var localStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var bcrypt = require('bcrypt');
 
 //REQUIRED ROUTES
@@ -82,6 +83,65 @@ passport.use('local', new localStrategy({passReqToCallback: true, usernameField:
     }));
 
 
+// =========================================================================
+// FACEBOOK ================================================================
+// =========================================================================
+var configAuth = require('./auth');
+
+passport.use(new FacebookStrategy({
+
+        // pull in our app id and secret from our auth.js file
+        clientID: configAuth.facebookAuth.clientID,
+        clientSecret: configAuth.facebookAuth.clientSecret,
+        callbackURL: configAuth.facebookAuth.callbackURL,
+        enableProof:true,
+        profileFields: ["emails", "displayName"]
+
+    },
+
+    // facebook will send back the token and profile
+    function (token, refreshToken, profile, done) {
+
+        // asynchronous
+        process.nextTick(function () {
+
+            // find the user in the database based on their facebook id
+            User.findOne({'facebook.id': profile.id}, function (err, user) {
+
+                // if there is an error, stop everything and return that
+                // ie an error connecting to the database
+                if (err)
+                    return done(err);
+
+                // if the user is found, then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    // if there is no user found with that facebook id, create them
+                    var newUser = new User();
+
+                    // set all of the facebook information in our user model
+                    newUser.faceid = profile.id; // set the users facebook id
+                    newUser.facetoken = token; // we will save the token that facebook provides to the user
+                    newUser.name = profile.displayName; // look at the passport user profile to see how names are returned
+                    newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+
+                    // save our user to the database
+                    newUser.save(function (err) {
+                        if (err)
+                            throw err;
+
+                        // if successful, return the new user
+                        return done(null, newUser);
+                    });
+                }
+
+            });
+        });
+
+    }));
+
+
 
 passport.serializeUser(function (user, done) {
     done(null, user.id)
@@ -100,6 +160,21 @@ passport.deserializeUser(function (id, done) {
         done(null, user)
     })
 });
+
+
+// =====================================
+// FACEBOOK ROUTES =====================
+// =====================================
+// route for facebook authentication and login
+app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+// handle the callback after facebook has authenticated the user
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect : '/confirmReservation',
+        failureRedirect : '/login'
+    })
+);
 
 
 //ROUTES
