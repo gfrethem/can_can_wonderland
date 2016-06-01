4d46fabd8e0da07e83881fb5ec671dc3a7585b40
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2016-06-01 14:04:28 -0500

    serverSettings.js file... again

--- serversettings.js
+++ serversettings.js
@@ -4,6 +4,18 @@ module.exports = {
         'clientID'      : '931135916980915', // your App ID
         'clientSecret'  : '991142356e58690fb4795682316b9985', // your App Secret
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
-    }
+    },
 
+    'twilioAuth' : {
+        'ACCOUNT_SID'   : 'ACa191532f90a93e915f16da74ef789a7a',
+        'AUTH_TOKEN'    : 'e9ccd52f2d96b3801435c108ca0470ba'
+    },
+    'mySQLAuth' : {
+        'serverAddress'     :   'jackswastedlife.org',
+        'serverPort'        :   '3306',
+        'dbName'            :   'gfrethem_cancan',
+        'userName'          :   'gfrethem_cancan',
+        'password'          :   'P!nk69'
+
+    }
 };
